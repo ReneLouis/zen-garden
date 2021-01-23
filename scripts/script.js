1e95967bfe629a3cfefce403ae121711b9acc022
René Louis <rglouis@hotmail.fr>
2021-01-23 16:06:51 +0000

    removed const that was not needed anymore.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 const MENU_OPTIONS = document.querySelector("nav > ul");
 const MENU_BUTTON = document.querySelector(".nav-btn");
-const MENU_LIST = document.querySelector(".menu");
 var timerID;
 
 function showMenu() {
